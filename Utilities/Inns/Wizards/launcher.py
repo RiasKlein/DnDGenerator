0f16f5dc78b79c10c8cfd9efd03040bec215c04b
Rias <tseshunman@gmail.com>
2016-06-26 02:41:18 -0400

    Wizards, Inn - Rumor extraction

--- Utilities/Inns/Wizards/launcher.py
+++ Utilities/Inns/Wizards/launcher.py
@@ -15,12 +15,34 @@ import os, sys, subprocess
 
 # Globals
 titleStrip_output = 'titleStrip_out.txt'
+rumors_output = 'rumors_out.txt'
+
+rumors_list = []
 
 def main():
 	checkArgs()			# Check the number of arguments to make sure we have an input file
 	
 	# Strip the title from the raw data (not required)
 	exec_titleStrip ()
+	
+	# Extract rumors from file into the list
+	extractRumors( sys.argv[1] )
+	
+# extractRumors
+#	Pulls rumors from the wizards raw data file
+def extractRumors( input ):
+	rfile = open ( input, 'r' )
+	wfile = open ( rumors_output, 'w' )
+	
+	while True:
+		line = rfile.readline()
+		if not line: 
+			rfile.close()
+			wfile.close()
+			return
+		
+		if 'Rumors' in line:
+			wfile.write(line)
 	
 # checkArgs
 #	Verifies that the number of arguments is acceptable
